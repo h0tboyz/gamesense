9d59cef2435ad249a6b335c0674397cae4b6b0d3
h0tboyz <39217394+h0tboyz@users.noreply.github.com>
2023-04-04 21:04:34 +0300

    Update rage_unsafe_recharge.lua
    s/o Shade

--- rage_unsafe_recharge.lua
+++ rage_unsafe_recharge.lua
@@ -6,6 +6,8 @@ local vars = {
 
         dt = ui.reference("RAGE", "Aimbot", "Double tap"),
         dt_key = select(2, ui.reference("RAGE", "Aimbot", "Double tap")),
+
+        flags = ui.reference("VISUALS", "Player ESP", "Flags"),
     },
     globals = {
         local_vulnerable = nil,
@@ -22,21 +24,9 @@ local func = {
             return 
         end
 
-        if entity.is_dormant(th) then 
-            vars.globals.local_vulnerable = false 
-            return 
-        end
-
-        local start_pos = {entity.hitbox_position(th, 0)}
-        local end_pos = {entity.get_prop(local_player, "m_vecOrigin")}
-        end_pos[3] = end_pos[3] + 32
-
-        local _, dmg = client.trace_bullet(th, start_pos[1], start_pos[2], start_pos[3], end_pos[1], end_pos[2], end_pos[3], false)
-
-        if dmg > 1 then
+        if (bit.band(entity.get_esp_data(th).flags, bit.lshift(1, 11)) == 2048) then 
             vars.globals.local_vulnerable = true 
-        else
-            vars.globals.local_vulnerable = false 
+            return
         end
     end,
 }
@@ -49,8 +39,6 @@ client.set_event_callback("setup_command", function(e)
     end 
 
     if bit.band(entity.get_prop(local_player, "m_fFlags"), 1) == 0 then 
-        func.update_vulnerable_state(local_player)
-
         if vars.globals.local_vulnerable == true then
             if antiaim_funcs.get_double_tap() == false then 
                 ui.set(vars.ref.aimbot, false)
@@ -60,6 +48,18 @@ client.set_event_callback("setup_command", function(e)
         end
     else
         ui.set(vars.ref.aimbot, true) 
+    end
+end)
+
+client.set_event_callback("paint", function()
+    local local_player = vars.globals.local_player
+    if local_player == nil then 
+        vars.globals.local_player = entity.get_local_player()
+        return
+    end 
+
+    if bit.band(entity.get_prop(local_player, "m_fFlags"), 1) == 0 then 
+        func.update_vulnerable_state(local_player)
     end
 end)
 
